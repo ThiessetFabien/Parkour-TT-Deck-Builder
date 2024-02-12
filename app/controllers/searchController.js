2d6199b3d0f0c5bece1705654d270b3582e3a107
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-02-12 12:42:47 +0100

    feet/link card on homepage

--- app/controllers/searchController.js
+++ app/controllers/searchController.js
@@ -3,9 +3,9 @@ const dataMapper = require("../dataMapper.js");
 const searchController = {
     searchPage: async (req, res) => {
         try {
-            const searchTerm = req.params.q;
-            const result = await dataMapper.search(searchTerm);
-            if (!result.length === 0) {
+            const searchTerm = req.body.q;
+            const result = await dataMapper.getSearch(searchTerm);
+            if (result.length === 0) {
                 res.status(200).send("Aucun résultat trouvé pour la recherche : " + searchTerm);
             } else {
                 console.log(result);
@@ -15,7 +15,6 @@ const searchController = {
             console.error(error, "Erreur lors de l'affichage de la recherche");
             res.status(500).send("Erreur lors de l'affichage de la recherche");
         }
-    }
-};
+    }};
 
 module.exports = searchController;
